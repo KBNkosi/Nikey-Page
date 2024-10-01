91034f90f438f2fef9c6fc52847b09a420c0b3e5
Nkosikenny <kennybhekumuzi@gmail.com>
2024-10-01 20:50:17 +0200

    created components

---
+++ src/sections/index.js
@@ -0,0 +1,10 @@
+import CustomerReviews from "./CustomerReviews"
+import PopularProducts from "./PopularProducts"
+import Services from "./Servcies"
+import SpecialOffer from "./SpecialOffer"
+import Subscribe from "./Subscribe"
+import SuperQuality from "./SuperQuality"
+import Hero from "./Hero"
+import Footer from "./Footer"
+
+export { CustomerReviews, PopularProducts, Services, SpecialOffer, Subscribe, SuperQuality, Hero, Footer }
